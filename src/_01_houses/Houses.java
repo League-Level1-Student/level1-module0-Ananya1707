488ddd25d5d8a2576479ba4f8b2a667c1ff51521
root <root@adf24e94afca>
2020-10-05 23:58:16 +0000

    Working on Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,34 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot robot = new Robot("mini");
+	
 	public void run() {
+		robot.penDown();
+		robot.setSpeed(100);
+		robot.setX(20);
+		robot.setY(550);
+		house(70);
 		
+		
+
+		
+	}
+	
+	public void house(int height) {
+		for(int i = 0; i < 11; i++) {
+			robot.setPenColor(0, 0, 0);
+			robot.move(height);
+			robot.turn(90);
+			robot.move(20);
+			robot.turn(90);
+			robot.move(height);
+			robot.turn(-90);
+			robot.setPenColor(0, 255, 0);
+			robot.move(30);
+			robot.turn(-90);
+		}
 	}
 }
