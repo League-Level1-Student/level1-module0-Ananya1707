db51fb416e070b3795e345e3cb9cde0a80a36501
root <root@3f0feca9259b>
2020-10-26 23:56:28 +0000

    Working on Fortune Cookie

--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -42,7 +42,7 @@ public class BodyPartQuiz {
 
 		// 3. Change the size of the window so that you can only see part of the
 		// image.
-		window.setSize(500, 500);
+		window.setSize(220, 130);
 
 		showNextImage();
 
@@ -51,6 +51,7 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
+		int score = 0;
 
 		// 2. Set the size of the window in the initializeGui() method 
 
@@ -59,15 +60,52 @@ public class BodyPartQuiz {
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
+		if(guess.equalsIgnoreCase("Arnold")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "CORRECT:)" + "\nScore: " + score);
+		}
 
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT:(" + "\nScore: " + score);
+		}
 
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
 
 		// 8. .... repeat 4-7 for all your images.....
+		String guess2 = JOptionPane.showInputDialog("who is this?");
+		if(guess2.equalsIgnoreCase("Leonardo")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "CORRECT:)" + "\nScore: " + score);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT:(" + "\nScore: " + score);
+		}
+		showNextImage();
+		
+		String guess3 = JOptionPane.showInputDialog("who is this?");
+		if(guess3.equalsIgnoreCase("Morgan")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "CORRECT:)" + "\nScore: " + score);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT:(" + "\nScore: " + score);
+		}
+		showNextImage();
+		
+		String guess4 = JOptionPane.showInputDialog("who is this?");
+		if(guess4.equalsIgnoreCase("Jack")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "CORRECT:)" + "\nScore: " + score);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT:(" + "\nScore: " + score);
+		}
+		showNextImage();
 
+		
 		// 9. Show them their current score
 
 	}
---
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -0,0 +1,13 @@
+package _07_fortune_cookie;
+
+import javax.swing.JFrame;
+
+public class FortuneCookie {
+	public void showButton() {
+		System.out.print("Button Clicked");
+		
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+	}
+
+}
